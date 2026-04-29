a71ee4462062d34b0249efd21abbb0ba511aa926
Bakhtiyar <xiaomitronkz@gmail.com>
2026-04-29 13:21:20 +0500

    fix cache headers

--- server.js
+++ server.js
@@ -27,6 +27,12 @@ const pool = new Pool({
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
 
 function validationError(res, message, context = {}) {
   console.warn('VALIDATION ERROR:', context);
