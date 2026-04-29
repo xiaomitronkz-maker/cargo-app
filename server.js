95c53dd13d97bea5fd9c0959fd15d240f416e270
Bakhtiyar <xiaomitronkz@gmail.com>
2026-04-29 12:24:08 +0500

    fix sqlite driver

--- server.js
+++ server.js
@@ -9,7 +9,8 @@
 'use strict';
 
 const express = require('express');
-const Database = require('better-sqlite3');
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('./cargo.db')
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
